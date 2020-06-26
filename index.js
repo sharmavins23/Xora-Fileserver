e1375ca045e2418c6513ae84a0ef8778e5d2103c
Vins <sharmavins23@gmail.com>
2020-06-26 19:45:38 -0400

    removed test

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-var express = require("express");
-var app = express();
-var fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 3000;
 
 // Dynamically load all routes from all project folders
 require("./routes")(app);
 
 // Server configuration
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Server is now listening at http://%s:%s", host, port);
+app.listen(port, () => {
+    console.log(`Server is now listening at http://localhost:${port}`);
 });
