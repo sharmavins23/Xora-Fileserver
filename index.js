c483f4e095ee3fad566378608e446080c630e9d6
Vins <sharmavins23@gmail.com>
2020-06-26 18:24:15 -0400

    🎉 initial setup

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var app = express();
+var fs = require("fs");
+
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Server is now listening at http://%s:%s", host, port);
+});
