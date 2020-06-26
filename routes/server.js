45af00ffc5a3fc47c459ef803081d88b9ded8735
Vins <sharmavins23@gmail.com>
2020-06-26 19:54:45 -0400

    🎉 baby's first working api route

--- routes/server.js
+++ routes/server.js
@@ -1,18 +1,18 @@
 // * This is an administrative file that lists other projects.
 
-const Filehound = require("filehound");
-const dataDir = "../data/";
+const fs = require("fs");
+const dataDir = "./data/";
 
 function server(app) {
     // Return a list of all projects.
     app.get("/", function (req, res) {
-        Filehound.create()
-            .path(dataDir)
-            .directory()
-            .find()
-            .then((subdirectories) => {
-                console.log(subdirectories);
-            });
+        res.status(200).send(JSON.stringify(getDirectories(dataDir)));
+    });
+}
+
+function getDirectories(path) {
+    return fs.readdirSync(path).filter(function (file) {
+        return fs.statSync(path + "/" + file).isDirectory();
     });
 }
 
