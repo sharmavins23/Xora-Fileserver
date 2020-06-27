374a3b755f3a7d60d76dbefc482df7589733dfa5
Vins <sharmavins23@gmail.com>
2020-06-27 00:49:19 -0400

    🔧 properly implemented hot reloading of data

--- routes/zoo.js
+++ routes/zoo.js
@@ -1,15 +1,16 @@
 // * Main Project: https://github.com/sharmavins23/Zoo
 
-let zooData = require("../data/zoo/zooData.json");
+const zooDataPath = "data/zoo/zooData.json";
+var Reloader = require("reload-json");
+var reload = new Reloader();
 
 function zoo(app) {
     // Get all animals from zoo.
     app.get("/zoo", (req, res) => {
-        // Hot reloading of JSON files
-        delete require.cache[require.resolve("./zooData.json")]; // Deleting loaded module
-        zooData = require("./config.json");
-
-        res.send(zooData);
+        // Hot reloading support for module - reloads on source file change
+        reload.load(zooDataPath, (err, data) => {
+            res.send(data);
+        });
     });
 
     // Add an animal to the zoo.
