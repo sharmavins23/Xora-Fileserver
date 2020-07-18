7e1963d7e8f16fa0204be4a6c52923081e964cb6
Vins <sharmavins23@gmail.com>
2020-07-17 21:43:03 -0400

    🔊 added project logging

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
 const port = 80;
 
 // TODO: Add project-related sub-readmes for API documentation
-// TODO: Add universal logging for various routes and transfer
 
 // Enable JSON POST requests
 app.use(express.json());
+// Enable console logging of HTTP requests
+app.use(morgan("dev"));
 
 // Dynamically load all routes from all project folders
 require("./routes")(app);
