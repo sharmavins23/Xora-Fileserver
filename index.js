6cd3cd0c013d36fbf0e16ae7c2d9c3ef40c9162e
Vins <sharmavins23@gmail.com>
2020-07-17 22:29:43 -0400

    📝 finished writing project-specific API docs

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const morgan = require("morgan");
 
 const port = 80;
 
-// TODO: Add project-related sub-readmes for API documentation
-
 // Enable JSON POST requests
 app.use(express.json());
 // Enable console logging of HTTP requests
