b7438cd1fee64fc8b67efa7cf18cbdbefa0d155f
Vins <sharmavins23@gmail.com>
2020-07-17 22:47:52 -0400

    🔒 added schema validation

--- routes/zoo.js
+++ routes/zoo.js
@@ -1,6 +1,7 @@
 // * Main Project: https://github.com/sharmavins23/Zoo
 
 const fs = require("fs");
+const { check, validationResult } = require("express-validator");
 
 const zooDataPath = "data/zoo/zooData.json";
 let zooData = reloadData();
@@ -9,20 +10,32 @@ function zoo(app) {
     // Get all animals from the zoo.
     app.get("/zoo", (req, res) => {
         reloadData(); // Hot reload in event of manual changes
-        res.send(zooData);
+        res.json(zooData);
     });
 
     // Add an animal to the zoo.
-    app.post("/zoo/add", (req, res) => {
-        // TODO: Schema Validation
-        reloadData();
+    app.post(
+        "/zoo/add",
+        [
+            check("name").exists(),
+            check("default_scalar").exists(),
+            check("URL").exists().isURL(),
+        ],
+        (req, res) => {
+            // Schema validation
+            const errors = validationResult(req);
+            if (!errors.isEmpty())
+                return res.status(400).json({ errors: errors.array() });
 
-        // Add animal data and write to file
-        zooData["assets"].push(req.body);
-        fs.writeFileSync(zooDataPath, JSON.stringify(zooData));
+            reloadData();
 
-        res.status(201).send({ Message: "Data successfully appended." });
-    });
+            // Add animal data and write to file
+            zooData["assets"].push(req.body);
+            fs.writeFileSync(zooDataPath, JSON.stringify(zooData));
+
+            res.status(201).json({ Message: "Data successfully appended." });
+        }
+    );
 
     // Flush the data.
     app.delete("/zoo/flush", (req, res) => {
